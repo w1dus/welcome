ae5250420cf02a24416a09f10ef72a1d702881d8
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-12-31 16:09:42 +0900

    헤더 퍼블

--- script/common_jp.js
+++ script/common_jp.js
@@ -30,17 +30,27 @@ const privacyUsePopup = () => {
     })
 }
 const bodyScroll = () => {
+    // 필요한 요소가 있는지 확인
+    const wrapper = document.querySelector("#smooth-wrapper");
+    const content = document.querySelector("#smooth-content");
+  
+    if (!wrapper || !content) {
+      // 요소 없으면 함수 실행 안 하고 종료
+      return;
+    }
+  
     gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
-
+  
     ScrollSmoother.create({
-        wrapper: "#smooth-wrapper",
-        content: "#smooth-content",
-        smooth: 1.2,         // ← 숫자 높을수록 감속이 강해짐
-        effects: true,        // 패럴럭스 같은 ScrollTrigger 효과 활성화
-        ease: "power4.out", // 더 강한 감속 (추천)
-        autoKill: false // 스크롤 중단 방지
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.2,         // ← 숫자 높을수록 감속이 강해짐
+      effects: true,       // 패럴럭스 같은 ScrollTrigger 효과 활성화
+      ease: "power4.out",  // 더 강한 감속 (추천)
+      autoKill: false      // 스크롤 중단 방지
     });
-}
+};
+  
 const headerScrollHandler = () => {
     /* header */
     const header_main = document.querySelector('header');
--- script/script.js
+++ script/script.js
@@ -2,6 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", function(e){
 
+  //## 우측 메뉴 슬라이드 ##
+  slideMenu()
+
 })
 
 
+const slideMenu = () => {
+  $('header .menu-btn').click(function(){
+    $('.slide-menu').addClass('show');
+    $('.close-bg').addClass('show');
+  })
+
+  $('.close-bg').click(function(){
+    $('.slide-menu').removeClass('show');
+    $('.close-bg').removeClass('show');
+  })
+
+  $('.slide-menu .btn-div .close-btn').click(function(){
+    $('.slide-menu').removeClass('show');
+    $('.close-bg').removeClass('show');
+  })
+}
