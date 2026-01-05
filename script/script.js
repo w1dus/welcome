eb43a334ad1d30b05247ce5c2d0a2f9cf1ec9997
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2026-01-05 16:37:58 +0900

    스크립트 및 이미지 오탈자 수정

--- script/script.js
+++ script/script.js
@@ -5,8 +5,54 @@ document.addEventListener("DOMContentLoaded", function(e){
   //## 우측 메뉴 슬라이드 ##
   slideMenu()
   index_section2_slide();
+  scroll_move();
+  applyform();
 })
 
+const applyform = () => {
+   // 이름: 한글만, 최대 7자
+   $('#name').on('input', function (e) {
+    // 한글 조합 중이면 return
+    if (e.originalEvent && e.originalEvent.isComposing) return;
+
+    let value = $(this).val();
+
+    // 한글만 허용
+    value = value.replace(/[^가-힣]/g, '');
+
+    // 최대 7자
+    value = value.substring(0, 7);
+
+    $(this).val(value);
+  });
+
+
+  // 연락처: 숫자만, 3-4-4 자동 하이픈
+  $('#phone').on('input', function () {
+    let value = $(this).val().replace(/[^0-9]/g, '');
+
+    if (value.length > 11) {
+      value = value.substring(0, 11);
+    }
+
+    if (value.length <= 3) {
+      value = value;
+    } else if (value.length <= 7) {
+      value = value.replace(/(\d{3})(\d+)/, '$1-$2');
+    } else {
+      value = value.replace(/(\d{3})(\d{4})(\d+)/, '$1-$2-$3');
+    }
+
+    $(this).val(value);
+  });
+}
+
+const scroll_move = () => {
+  $(".scroll_move").click(function(e){  
+    e.preventDefault();       
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+  });
+}
 
 const index_section2_slide= () => {
   var swiper = new Swiper(".main .section2 .mySwiper", {
